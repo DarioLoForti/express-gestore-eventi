00b7c3b8823609f2c29d18d897433aca97c8841e
Dario Lo Forti <lofortidario@gmail.com>
2024-05-31 12:06:41 +0200

    Bonus Step 1: Add reservation routes and controller for event reservations

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const eventController = require('../controllers/eventController.js');
+const reservationController = require('../controllers/reservationController.js');
 
 router.get('/', eventController.index);
 router.post('/', eventController.store);
 router.put('/:event', eventController.update);
+
+
+router.get('/:event/reservations', reservationController.index);
+router.post('/:event/reservations', reservationController.store);
+router.delete('/:event/reservations/:reservation', reservationController.destroy);
 
 module.exports = router;
