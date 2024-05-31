fe0210aaaa7f4e4aa32eda81dc8fc5d1566ba46d
Dario Lo Forti <lofortidario@gmail.com>
2024-05-31 12:09:29 +0200

    Bonus Step 2: Add Reservation model for event reservations

---
+++ models/reservation.js
@@ -0,0 +1,10 @@
+class Reservation {
+
+    constructor(id, eventId, firstName, lastName, email ) {
+        this.id = id;
+        this.eventId = eventId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+}
